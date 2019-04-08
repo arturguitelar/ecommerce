e02aee4f87988f2d2eeb0894224f61cd53aa23a0
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-08 17:25:10 -0300

    Alterar a senha pelo profile do site.

--- routes/site-profile.php
+++ routes/site-profile.php
@@ -102,3 +102,61 @@ $app->get("/profile/orders/:idorder", function($idorder) {
 		'products' => $cart->getProducts()
     ]);
 });
+
+$app->get("/profile/change-password", function() {
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		'changePassError' => User::getMsgError(),
+		'changePassSuccess' => User::getMsgSuccess()
+	]);
+});
+
+$app->post("/profile/change-password", function() {
+	User::verifyLogin(false);
+
+	// se o usuário digitou a senha
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+		User::setMsgError("Digite a senha atual.");
+		header('Location: /profile/change-password');
+		exit;
+	}
+
+	// nova senha e confrmação da nova senha
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+		User::setMsgError("Digite a nova senha.");
+		header('Location: /profile/change-password');
+		exit;
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+		User::setMsgError("Confirme a nova senha.");
+		header('Location: /profile/change-password');
+		exit;
+	}
+
+	// verifica se a senha atual é diferente da nova
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+		User::setMsgError("A sua nova senha deve ser diferente da atual.");
+		header('Location: /profile/change-password');
+		exit;
+	}
+
+	// verifica se a senha atual do usuário está correta
+	$user = User::getFromSession();
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+		User::setMsgError("A senha está inválida.");
+		header('Location: /profile/change-password');
+		exit;
+	}
+
+	// caso tudo tenha ocorrido bem
+	$user->setdespassworg($_POST['new_pass']);
+	$user->update();
+	User::setMsgSuccess("Senha alterada com sucesso.");
+
+	header('Location: /profile/change-password');
+	exit;
+});
